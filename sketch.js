ad9148f59472ba92dc4a1fba836927d21bd5b587
23170373 <158121527+23170373@users.noreply.github.com>
2025-04-02 22:56:57 +0000

    Version 1.0

--- sketch.js
+++ sketch.js
@@ -1,10 +1,26 @@
+let clasificador
+let imagen
+
 function preload() {
   // put preload code here
+  clasificador = ml5.imageClassifier('MobileNet')
+  imagen = loadImage("./images/PastorAleman.jpg")
 }
 
 function setup() {
   // put setup code here
   createCanvas(800,800)
+  clasificador.classify(imagen, gotResult)
+  image(imagen, 0, 0, width, height)
+}
+
+function gotResult(results) {
+  print(results)
+  fill(255)
+  stroke(0)
+  textSize(18)
+  text(results[0].label, 10, 20)
+  text(results[0].confidence, 10, 40)
 }
 
 function draw() {
