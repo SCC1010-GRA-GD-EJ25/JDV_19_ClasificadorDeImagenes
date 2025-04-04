2e36bd121c34c66ef33d0afa374bbafd8ddef541
23170373 <158121527+23170373@users.noreply.github.com>
2025-04-04 00:32:41 +0000

    Version 1.1

--- sketch.js
+++ sketch.js
@@ -1,30 +1,49 @@
 let clasificador
 let imagen
+let video
+let etiqueta
+let confianza
 
 function preload() {
   // put preload code here
-  clasificador = ml5.imageClassifier('MobileNet')
+  clasificador = ml5.imageClassifier("MobileNet")
   imagen = loadImage("./images/PastorAleman.jpg")
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800,800)
-  clasificador.classify(imagen, gotResult)
-  image(imagen, 0, 0, width, height)
+  createCanvas(640,480)
+  //clasificador.classify(imagen, gotResult)
+  //image(imagen,0,0,width,height)
+  video = createCapture(VIDEO)
+  video.size(640,480)
+  //video.hide()
+  cladificaVideo()
 }
 
 function gotResult(results) {
-  print(results)
+  console.log(results[0].label)
   fill(255)
   stroke(0)
-  textSize(18)
+  textSize(32)
   text(results[0].label, 10, 20)
-  text(results[0].confidence, 10, 40)
+  text(results[0].confidence, 10, 80)
+  etiqueta = results[0].label
+  confianza = results[0].confidence
+  cladificaVideo()
 }
 
 function draw() {
   // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+  //background(255)
+  //rect(100,100,200,200)
+  tint(0,255,0)
+  image(video,0,0)
+  text(etiqueta, 10, 20)
+  text(confianza, 10, 80)
+}
+
+function cladificaVideo() {
+  clasificador.classify(video, gotResult)
+  console.log("Clasificando video")
 }
